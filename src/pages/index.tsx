2a9d533433ddcfe7122b950922e1392441a92b2e
SuperXuan1997@163.com <SuperXuan1997@163.com>
2021-01-13 10:54:41 +0800

    Image组件显示bug

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './index.less';
+import { Image } from 'antd';
+export default () => {
+  return (
+    <div>
+      <h1 className={styles.title}>Page index</h1>
+      <Image src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg" />
+    </div>
+  );
+};
